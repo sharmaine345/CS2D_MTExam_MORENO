5f35ea90d2068d7e0cc01991a32a5041af032425
sharmaine345 <sharmaineannemoreno7@gmail.com>
2024-10-31 18:58:16 +0800

    classSubject.js

---
+++ classSubject.js
@@ -0,0 +1,11 @@
+// Collecting class information
+
+let subjectTitle = "DSA";          
+let classSchedule = "MTTHF 8-9:30 AM - 11:00 AM-12:30PM"; 
+let classroom = "Com lab B";              
+let classInstructor = " Sir Cliff Owen Pascua";
+let studentName = "Sharmaine Anne Moreno";      
+
+// Logging information to the console
+
+console.log(`${studentName} is currently enrolled in ${subjectTitle} with a class schedule of ${classSchedule} at room ${classroom}. The instructor for the subject is ${classInstructor}`);
